70029a87b09f908840c7d516ecc09439c01aa8c5
stefanieliang <751179350@qq.com>
2019-02-20 15:20:50 +0800

    2-20

--- javascript/service.js
+++ javascript/service.js
@@ -1,13 +1,6 @@
 $(document).ready(function(){
-    $(".service-content li").mouseenter(function(e){
-        $(e.target).css('cursor','pointer');
-        const src = e.target.getElementsByTagName('img')[0].src;
-        const hoverSrc = src.split('.')[0]+'-hover.'+src.split('.')[1];
-        e.target.getElementsByTagName('img')[0].src = hoverSrc;
-    });
-    $(".service-content li").mouseleave(function(e){
-        const hoverSrc = e.target.getElementsByTagName('img')[0].src;
-        const src = hoverSrc.split('-hover.')[0]+'.'+hoverSrc.split('.')[1];
-        e.target.getElementsByTagName('img')[0].src = src;
-    });
+    $(".service-content li").on("click",function(){
+        var flag = $(this).attr("data-content");
+        window.location.href="/order/go/"+flag;
+    })
 });
